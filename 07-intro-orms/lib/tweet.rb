323cb52960cb0bc1515ea840b4e3deb3dee7d0e9
Steven Doran <steven.doran@flatironschool.com>
2019-08-15 14:54:36 -0400

    intro orm pt 2

--- 07-intro-orms/lib/tweet.rb
+++ 07-intro-orms/lib/tweet.rb
@@ -13,27 +13,45 @@ class Tweet
 
     results = DB[:conn].execute(sql)
 
+    # how do we turn this array of hashes into an array of Tweets
+
     results.map do |row|
       # Tweet.new({"message" => row["message"], "username" => row["username"]})
       Tweet.new(row) # why would this also work?
     end
- 
-  
-    # how do we turn this array of hashes into an array of Tweets
   end
 
   def initialize(props={})
     @message = props['message']
     @username = props['username']
 
-    @id = prop["id"] # why might we want to add the id attribute?
+    @id = props["id"] # why might we want to add the id attribute?
   end
 
   def save
     # @@all << self
     # self
 
+    username = self.username
+    message = self.message
+
     #what SQL would we want to implement to save Tweet instances to the database
+    
+    # HEREDOC 
+    sql = <<-SQL
+      INSERT INTO tweets (username, message)
+      VALUES (?, ?);
+    SQL
+    
+
+    DB[:conn].execute(sql, username, message)
   end
   
+  def update
+    # what SQL could we implement to change the message of a tweet
+  end
+
+  def delete
+    # what SQL could we implement to delete a row from the tweets table
+  end
 end
