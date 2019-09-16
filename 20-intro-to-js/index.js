20255ade74e9fad5d6a04a7f372a2626af731620
sbal13 <sbal13@gmail.com>
2019-09-16 11:49:37 -0400

    after intro to JS

---
+++ 20-intro-to-js/index.js
@@ -0,0 +1,8 @@
+console.log('%c HELLO WORLD', 'color: green')
+console.log('%c HELLO WORLD', 'color: red')
+console.log('%c HELLO WORLD', 'color: blue')
+console.log('%c HELLO WORLD', 'color: pink')
+
+var hello = "goodbye"
+
+
